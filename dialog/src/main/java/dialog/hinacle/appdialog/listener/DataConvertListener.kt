2f68210dafa8745a4b30912ec530b777ce15cb5b
Hinacle <workerlibai@163.com>
2022-07-22 13:10:15 +0800

    Initial commit

---
+++ dialog/src/main/java/dialog/hinacle/appdialog/listener/DataConvertListener.kt
@@ -0,0 +1,37 @@
+package dialog.hinacle.appdialog.listener
+
+import android.os.Parcel
+import android.os.Parcelable
+import dialog.hinacle.appdialog.AppDialog
+import dialog.hinacle.appdialog.other.ViewHolder
+
+abstract class DataConvertListener : Parcelable {
+
+    abstract fun convertView(dialogBinding: Any, dialog: AppDialog)
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    override fun writeToParcel(dest: Parcel, flags: Int) {}
+
+    constructor() {}
+
+    protected constructor(source: Parcel) {}
+
+    companion object {
+        val CREATOR: Parcelable.Creator<DataConvertListener> = object : Parcelable.Creator<DataConvertListener> {
+            override fun createFromParcel(source: Parcel): DataConvertListener {
+                return object : DataConvertListener(source) {
+                    override fun convertView(dialogBinding: Any, dialog: AppDialog) {
+
+                    }
+                }
+            }
+
+            override fun newArray(size: Int): Array<DataConvertListener?> {
+                return arrayOfNulls(size)
+            }
+        }
+    }
+}
